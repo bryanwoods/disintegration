eda1ff10ee8d26c2433137d1e62ef0a57ccba8aa
Bryan Woods <bryanwoods4e@gmail.com>
2015-12-01 17:39:19 -0500

    Gross code allows the poems to be rendered dynamically as a PNG

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -1,21 +1,57 @@
+require 'RMagick'
+
 class ProjectController < ApplicationController
-  before_filter :set_project
+  before_filter :set_project, :delete_frame_options_header
 
   def index
-    response.headers.delete("X-Frame-Options")
   end
 
-  def destroy
-    body = @project.body
-    return head(:ok) unless index = body.chars.find_index(params[:id])
+  def poems
+    respond_to do |format|
+      format.png do
+        body = @project.body
 
-    @project.update_attributes(body: body.tap { |_body| _body[index] = " " })
-    head :ok
+        random_character = body.chars.reject do
+          |char| [" ", "\n"].include?(char)
+        end.sample
+
+        index = body.chars.find_index(random_character) or
+          return render :index
+
+        @project.update_attributes(body: body.tap { |_body| _body[index] = " " })
+
+        write_image
+
+        send_file(
+          'public/images/poems.png',
+          type: 'image/png',
+          disposition: 'inline'
+        )
+      end
+    end
   end
 
   private
 
   def set_project
-    @project ||= Project.first
+    @project ||= Project.last
+  end
+
+  def delete_frame_options_header
+    response.headers.delete("X-Frame-Options")
+  end
+
+  def write_image
+    canvas = Magick::Image.new(500, 1700) do |c|
+      c.background_color = "Transparent"
+    end
+
+    gc = Magick::Draw.new
+
+    gc.pointsize(15)
+    gc.text(50, 50, @project.body)
+    gc.draw(canvas)
+
+    canvas.write("#{Rails.root}/public/images/poems.png")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :project, only: %i(index destroy)
+  get "/poems", to: "project#poems"
   root to: "project#index"
 end
